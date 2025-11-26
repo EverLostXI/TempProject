87dd8c07258f6deaf5348d45e7def370821019e8
wara <joewww233@gmail.com>
2025-11-26 20:04:23 +0800

    客户端登录初步实现

---
+++ Client/RegisterDialog.h
@@ -0,0 +1,29 @@
+#ifndef REGISTERDIALOG_H
+#define REGISTERDIALOG_H
+
+#include <QDialog>
+
+namespace Ui {
+class RegisterDialog;
+}
+
+class RegisterDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit RegisterDialog(QWidget *parent = nullptr);
+    ~RegisterDialog();
+
+private slots:
+    // "注册" 按钮的槽函数
+    void on_registerButton_clicked();
+
+    // "返回" 按钮的槽函数
+    void on_backButton_clicked();
+
+private:
+    Ui::RegisterDialog *ui;
+};
+
+#endif // REGISTERDIALOG_H
